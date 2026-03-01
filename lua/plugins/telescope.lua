14c8652cfe527072813fae52b8f8cc9c8c6b4b7a
matteofortino <matteofortino04@gmail.com>
2026-03-01 03:28:08 +0100

    fix: modify descriptions of commands
    The descriptions where all the same due to copy-paste distraction so I
    changed them correctly

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,14 @@
 return {
-    'nvim-telescope/telescope.nvim', version = '*',
+    'nvim-telescope/telescope.nvim',
+    version = '*',
     dependencies = {
         'nvim-lua/plenary.nvim',
         -- optional but recommended
         { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
-    config = function ()
-    	vim.keymap.set("n", "<leader>ff", ":Telescope fd<CR>", { desc = "Find Files" })
-    	vim.keymap.set("n", "<leader>fg", ":Telescope grep_string<CR>", { desc = "Find Files" })
-    	vim.keymap.set("n", "<leader>fs", ":Telescope lsp_document_symbols<CR>", { desc = "Find Files" })
+    config = function()
+        vim.keymap.set("n", "<leader>ff", ":Telescope fd<CR>", { desc = "Find Files" })
+        vim.keymap.set("n", "<leader>fg", ":Telescope grep_string<CR>", { desc = "Grep strings" })
+        vim.keymap.set("n", "<leader>fs", ":Telescope lsp_document_symbols<CR>", { desc = "Lsp symbols" })
     end
 }
