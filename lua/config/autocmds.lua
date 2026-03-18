a683f35bc5ccef051ed45472adb728f951786e29
matteofortino <matteofortino04@gmail.com>
2026-03-18 17:14:48 +0100

    feat: aggiunto autocomando per compilare i file latex
    Quando viene salvato un file con estensione .tex lo compila direttamente

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,9 +1,24 @@
 -- highlight_yank
 vim.api.nvim_create_autocmd("TextYankPost", {
-	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
-	pattern = "*",
-	desc = "highlight selection on yank",
-	callback = function()
-		vim.highlight.on_yank({ timeout = 200, visual = true })
-	end,
+    group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+    pattern = "*",
+    desc = "highlight selection on yank",
+    callback = function()
+        vim.highlight.on_yank({ timeout = 200, visual = true })
+    end,
+})
+
+-- latex document compilation
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.tex",
+    callback = function()
+        local file = vim.fn.expand("%:p")
+        local dir = vim.fn.expand("%:p:h")
+
+        vim.fn.jobstart({ "latexmk", "-pdf", file }, {
+            cwd = dir,
+            stdout_buffered = true,
+            stderr_buffered = true,
+        })
+    end,
 })
