dc9a43e517cbce7060b7e0f029a78852162f9435
matteofortino <matteofortino04@gmail.com>
2026-04-13 17:44:49 +0200

    feat: removed compile on save autocommands for .tex files

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -7,18 +7,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank({ timeout = 200, visual = true })
     end,
 })
-
--- latex document compilation
-vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = "*.tex",
-    callback = function()
-        local file = vim.fn.expand("%:p")
-        local dir = vim.fn.expand("%:p:h")
-
-        vim.fn.jobstart({ "latexmk", "-pdf", file }, {
-            cwd = dir,
-            stdout_buffered = true,
-            stderr_buffered = true,
-        })
-    end,
-})
