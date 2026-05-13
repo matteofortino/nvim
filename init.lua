a89ef52b4b3a948d49681996d82ef078bb568aef
matteofortino <matteofortino04@gmail.com>
2026-05-14 00:03:35 +0200

    feat: add ui2:

--- init.lua
+++ init.lua
@@ -1,6 +1,14 @@
+require("vim._core.ui2").enable({
+    enable = true,
+    msg = {
+        target = "cmd",
+        pager = { height = 0.5 },
+        dialog = { height = 0.5 },
+        cmd = { height = 0.5 },
+        msg = { height = 0.5, timeout = 45000 },
+    },
+})
 require("config.options")
 require("config.lazy")
 require("config.keymaps")
 require("config.autocmds")
-
-
