eecec9f1c79f47880ef11e6561e5cb9bca7064b5
matteofortino <matteofortino04@gmail.com>
2026-07-21 19:02:32 +0200

    change: change to mini.snipptes

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -1,21 +1,30 @@
 return {
-    {
-        "L3MON4D3/LuaSnip",
-        dependencies = {
-            "rafamadriz/friendly-snippets",
-        },
-        build = "make install_jsregexp",
+	{
+		"echasnovski/mini.snippets",
+		version = "*",
+		dependencies = {
+			-- Snippet collection covering JS/TS, Python, Go, Rust, C++, Lua, HTML, etc.
+			"rafamadriz/friendly-snippets",
+		},
+		config = function()
+			local mini_snippets = require("mini.snippets")
 
-        config = function()
-            local luasnip = require("luasnip")
+			mini_snippets.setup({
+				snippets = {
+					-- Automatically loads language snippets from runtime paths (friendly-snippets)
+					mini_snippets.gen_loader.from_lang(),
+				},
+				mappings = {
+					expand = "<C-j>", -- Expand snippet at cursor
+					jump_next = "<C-l>", -- Jump to next snippet field
+					jump_prev = "<C-h>", -- Jump to previous snippet field
+					stop = "<C-c>", -- Stop snippet session
+				},
+			})
 
-            -- carica snippet vscode-style (friendly-snippets)
-            require("luasnip.loaders.from_vscode").lazy_load()
-
-            -- opzionale: snippet custom LaTeX
-            require("luasnip.loaders.from_lua").load({
-                paths = "~/.config/nvim/lua/snippets/",
-            })
-        end,
-    },
+			-- Starts an in-process LSP server so your autocompletion plugin
+			-- (nvim-cmp, blink.cmp, mini.completion, etc.) displays snippets automatically
+			mini_snippets.start_lsp_server()
+		end,
+	},
 }
